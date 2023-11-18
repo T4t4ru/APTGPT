f15d3cf158aac39daeafcfa73817f28d6315542c
T4t4ru <100865104+T4t4ru@users.noreply.github.com>
2023-11-18 17:55:33 +0300

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+[ ! -f pretrain_transformers.py ] && wget https://raw.githubusercontent.com/EvilFreelancer/ru-gpts/master/pretrain_transformers.py
+
+base_model="sberbank-ai/rugpt3small_based_on_gpt2"
+output_dir="processed_pdfs_2023.csv"
+
+python3 pretrain_transformers.py \
+  --output_dir=${output_dir} \
+  --model_type=gpt2 \
+  --model_name_or_path=${base_model} \
+  --do_train \
+  --train_data_file=data/train.txt \
+  --do_eval \
+  --fp16 \
+  --eval_data_file=data/valid.txt \
+  --per_gpu_train_batch_size 4 \
+  --gradient_accumulation_steps 8 \
+  --num_train_epochs 5 \
+  --block_size 512 \
+  --overwrite_output_dir
